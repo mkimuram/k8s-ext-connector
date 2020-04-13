8d092bafec056315c5047314dd3467bdef55a833
Masaki Kimura <masaki.kimura@hitachivantara.com>
2020-04-13 15:26:25 +0000

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+deploy_dir="deploy"
+files=$(cat << EOF
+service_account.yaml
+role.yaml
+role_binding.yaml
+crds/submariner.io_externalconnectors_crd.yaml
+crds/submariner.io_externalservices_crd.yaml
+crds/submariner.io_forwarders_crd.yaml
+crds/submariner.io_gateways_crd.yaml
+operator.yaml
+EOF
+)
+
+if [ "$1" != "-u" ];then
+	for file in ${files};do
+		kubectl create -f ${deploy_dir}/${file}
+	done
+else
+	echo "${files}" | tac - | while read file;do
+		kubectl delete -f ${deploy_dir}/${file}
+	done
+fi
